44d8e26e98910a9bf00e1b779aacadc773867810
PLANALP Donald Rex <dopl4670@terra.colorado.edu>
2025-05-13 14:29:15 -0600

    cleaned up block plot

--- scripts/plot_block.py
+++ scripts/plot_block.py
@@ -2,31 +2,30 @@
 import numpy as np
 import matplotlib.pyplot as plt
 from matplotlib.colors import LogNorm, Normalize
-
 import json
 
+
+# Set norm to either Log or Linear
 
 with open("input.json") as f:
     data = json.load(f)
 
-
 lmax = data["Angular"]["lmax"]
-lm_to_block_txt = np.loadtxt("misc/lm_map.txt")
-probabilities_txt = np.loadtxt("misc/block_norms.txt")
+
+lmMap = np.loadtxt("misc/lm_map.txt")
+blockNorms = np.loadtxt("misc/block_norms.txt")
 
 fig,ax = plt.subplots(figsize=(10, 8))
-space_size =lmax + 1
+space_size = lmax + 1
 space = np.zeros((space_size, 2 * lmax + 1))
 
-column1 = lm_to_block_txt[:,0]
-column2 = lm_to_block_txt[:,1]
-column3 = lm_to_block_txt[:,2]
+lvalues = lmMap[:,0]
+mvalues = lmMap[:,1]
 
-
-column4 = probabilities_txt[:,0]
-column5 = probabilities_txt[:,1]
-for i in range(len(column1)):
-    space[lmax - int(column1[i]), int(column2[i]) + lmax] = column5[i]
+blockIndices = blockNorms[:,0]
+probVals = blockNorms[:,1]
+for i in range(len(lvalues)):
+    space[lmax - int(lvalues[i]), int(mvalues[i]) + lmax] = probVals[i]
 
 space[space==0] = np.min(space[space!=0])
 cax = ax.imshow(space, cmap='inferno', interpolation='nearest', norm=Normalize())  
@@ -37,8 +36,7 @@ ax.set_xticklabels([-lmax, 0, lmax])
 ax.set_yticks([0, lmax])  
 ax.set_yticklabels([lmax, 0])  
 ax.tick_params(axis='both', which='both', bottom=False, top=False, left=False, right=False) 
-
 fig.colorbar(cax, ax=ax, shrink=0.5)
 ax.set_title('Heatmap of Probabilities for l and m Values')
-fig.savefig("images/pyramid.png",dpi = 200)
+fig.savefig("images/block_norms.png",dpi = 200)
 fig.clf()
